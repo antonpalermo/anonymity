b0ed785aea896720cf8ac4c02cfc9d9258322ed6
antonpalermo <anpbusinessmail@gmail.com>
2023-11-19 22:46:56 +0800

    auth: prisma adapter

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,17 +1,24 @@
 import { NextAuthOptions } from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 
+import prisma from "../../../../db/prisma"
 import GoogleProvider from "next-auth/providers/google"
 
 /**
  * https://next-auth.js.org/configuration/options
  */
 const option: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET
     })
-  ]
+  ],
+  session: {
+    // override session strategy
+    strategy: "jwt"
+  }
 }
 
 export default option
