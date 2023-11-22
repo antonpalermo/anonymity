5335c33d45ca93c4c0293d9e413092d5bded50c5
antonpalermo <anpbusinessmail@gmail.com>
2023-11-22 17:51:04 +0800

    add redirects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function RootPage() {
-  return <h1 className="text-green-400">Sample</h1>
+import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth"
+
+import prisma from "@/db/prisma"
+import option from "@/app/api/auth/[...nextauth]/options"
+
+export default async function RootPage() {
+  const {
+    user: { email }
+  } = await getServerSession(option)
+  const team = await prisma.team.findFirst({
+    where: {
+      members: { every: { user: { email } } }
+    }
+  })
+
+  if (!team) {
+    redirect("/create")
+  }
+
+  redirect(`/${team.id}`)
 }
