f04895bab944cb2c55da98fe53ea47bd10cab42c
antonpalermo <anpbusinessmail@gmail.com>
2023-11-22 22:49:02 +0800

    add checker for non existing teams

--- src/app/(dashboard)/[id]/layout.tsx
+++ src/app/(dashboard)/[id]/layout.tsx
@@ -1,8 +1,20 @@
-export default function DashboardLayout({
-  children
+import { notFound } from "next/navigation"
+
+import prisma from "@/db/prisma"
+
+export default async function DashboardLayout({
+  children,
+  params: { id }
 }: {
   children: React.ReactNode
+  params: { id: string }
 }) {
+  const team = await prisma.team.findUnique({ where: { id } })
+
+  if (!team) {
+    notFound()
+  }
+
   return (
     <div>
       <h1>Dashboard Layout</h1>
