ba4817909e042feb776f9daf9d4eaf88abc8fb24
antonpalermo <anpbusinessmail@gmail.com>
2024-01-01 21:31:35 +0800

    persist user role and teamId

--- src/app/(dashboard)/[id]/page.tsx
+++ src/app/(dashboard)/[id]/page.tsx
@@ -1,3 +1,8 @@
-export default function DashboardPage() {
-  return <h1>Dashboard</h1>
+import { getServerSession } from "next-auth"
+import option from "@/app/api/auth/[...nextauth]/options"
+
+export default async function DashboardPage() {
+  const { user } = await getServerSession(option)
+
+  return <h1>{JSON.stringify(user)}</h1>
 }
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -18,6 +18,31 @@ const option: NextAuthOptions = {
   session: {
     // override session strategy
     strategy: "jwt"
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        const access = await prisma.member.findFirst({
+          where: { user: { id: user.id } }
+        })
+        // check if the user already have a team.
+        if (access) {
+          token.tid = access.teamId
+          token.role = access.role
+        }
+
+        token.id = user.id
+      }
+
+      return Promise.resolve(token)
+    },
+    session: async ({ session, token }) => {
+      session.user.id = token.id
+      session.user.tid = token.tid // team id
+      session.user.role = token.role
+
+      return Promise.resolve(session)
+    }
   }
 }
 
