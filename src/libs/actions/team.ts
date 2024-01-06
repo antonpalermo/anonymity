c00a2a727b3422ed33e2818f92da88fe68dcc24f
antonpalermo <anpbusinessmail@gmail.com>
2024-01-06 23:44:31 +0800

    refac: add unique constrains to team name

--- src/libs/actions/team.ts
+++ src/libs/actions/team.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client"
 import { redirect } from "next/navigation"
 
 import prisma from "@/db/prisma"
@@ -7,16 +8,58 @@ import prisma from "@/db/prisma"
  * @param email email address of the member
  */
 async function find(email: string) {
-  const team = await prisma.team.findFirst({
-    where: { members: { some: { user: { email } } } },
-    include: { members: { select: { role: true, user: true } } }
-  })
+  // team data
+  let team: Prisma.TeamGetPayload<{
+    include: { members: { select: { role: true; user: true } } }
+  }>
 
-  if (!team) {
-    redirect(`/team/create`)
+  try {
+    // find the first team and assign it to team variable.
+    team = await prisma.team.findFirst({
+      where: { members: { some: { user: { email } } } },
+      include: { members: { select: { role: true, user: true } } }
+    })
+  } catch (e) {
+    // console log the error.
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log({ code: e.code, message: e.message })
+    }
   }
 
+  if (!team) {
+    // if there's no team available redirect them to
+    // create team page.
+    redirect(`/team/create`)
+  }
+  // return the team data if exists.
   return team
 }
 
-export default { find }
+async function create(name: string, admin: string) {
+  // team data
+  let team: Prisma.TeamGetPayload<{
+    include: { members: { select: { role: true; user: true } } }
+  }>
+
+  try {
+    team = await prisma.team.create({
+      data: {
+        name,
+        members: {
+          create: { user: { connect: { email: admin } }, role: "admin" }
+        }
+      },
+      include: { members: { select: { role: true, user: true } } }
+    })
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return { message: `${name} is already taken` }
+      }
+    }
+  }
+
+  return create
+}
+
+export default { find, create }
