209337260e1735a40226f8676569f209d8ef629c
antonpalermo <anpbusinessmail@gmail.com>
2024-01-12 22:28:13 +0800

    added tiptap

--- src/app/(incident)/incident/create/page.tsx
+++ src/app/(incident)/incident/create/page.tsx
@@ -1,3 +1,10 @@
+import Editor from "@/components/editor"
+
 export default function CreateIncidentPage() {
-  return <h1>Create Incident Report</h1>
+  return (
+    <div>
+      <h1>Create Incident Report</h1>
+      <Editor />
+    </div>
+  )
 }
---
+++ src/components/editor.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { useEditor, EditorContent } from "@tiptap/react"
+
+import StarterKit from "@tiptap/starter-kit"
+
+export default function Editor() {
+  const editor = useEditor({
+    extensions: [StarterKit]
+  })
+
+  return (
+    <div>
+      <EditorContent editor={editor} />
+    </div>
+  )
+}
