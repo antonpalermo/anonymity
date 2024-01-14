5f75f093c11d404b4651df3900480602123c9b14
antonpalermo <anpbusinessmail@gmail.com>
2024-01-14 23:27:17 +0800

    setup editor contents

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,7 +1,13 @@
 "use client"
 
 import { KeyboardEvent, useEffect } from "react"
-import { useEditor, EditorContent, JSONContent } from "@tiptap/react"
+import {
+  useEditor,
+  EditorContent,
+  JSONContent,
+  Editor as TiptapEditor,
+  EditorEvents
+} from "@tiptap/react"
 
 import { PreventNewLine } from "@/components/editor/extensions"
 
@@ -10,12 +16,19 @@ import Heading from "@tiptap/extension-heading"
 import Document from "@tiptap/extension-document"
 import StarterKit from "@tiptap/starter-kit"
 import Placeholder from "@tiptap/extension-placeholder"
+import { Transaction } from "@tiptap/pm/state"
 
 interface EditorProps {
   editable?: boolean
+  heading: (heading: string) => void
+  contents: (contents: JSONContent) => void
 }
 
-export default function Editor({ editable = true }: EditorProps) {
+export default function Editor({
+  heading,
+  contents,
+  editable = true
+}: EditorProps) {
   const headingEditor = useEditor({
     extensions: [
       Text,
@@ -30,7 +43,8 @@ export default function Editor({ editable = true }: EditorProps) {
         levels: [1]
       })
     ],
-    editable
+    editable,
+    onUpdate: onUpdateHeading
   })
 
   const contentEditor = useEditor({
@@ -42,8 +56,19 @@ export default function Editor({ editable = true }: EditorProps) {
         heading: { levels: [2] }
       })
     ],
-    editable
+    editable,
+    onUpdate: onUpdateContent
   })
+
+  function onUpdateHeading({ editor }: EditorEvents["update"]) {
+    if (!headingEditor || !contentEditor) return
+    heading(editor.getText())
+  }
+
+  function onUpdateContent({ editor }: EditorEvents["update"]) {
+    if (!headingEditor || !contentEditor) return
+    contents(editor.getJSON())
+  }
 
   function handleHeadingKeydown(event: KeyboardEvent<HTMLDivElement>): void {
     if (!headingEditor || !contentEditor) return
